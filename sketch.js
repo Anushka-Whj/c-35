85728c5f0cf3811589ff05cb56d6724d016660e3
Anushka Rao <bittushka1968@gmail.com>
2021-02-06 11:48:20 +0530

    i lernt about database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
 var ball;
+var database;
+var dBball;
+var position;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    var dBballPosition = database.ref('ball/position');
+    dBballPosition.on("value",readPosition,showError)
 }
 
 function draw(){
@@ -24,6 +32,15 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({ 'x': position.x + x , 'y': position.y + y })
+}
+
+function readPosition(data) {
+position = data.val()
+ball.x=position.x
+ball.y=position.y
+}
+
+function showError(){
+console.log("some error occured")
 }
